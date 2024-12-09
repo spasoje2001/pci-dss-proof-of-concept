29ea055fda54bc7fe264dd4e3ef41bba04bf2862
Spasoje <spasoje.brboric@gmail.com>
2024-12-09 22:56:23 +0100

    https setup

--- go-pci-dss/main.go
+++ go-pci-dss/main.go
@@ -19,9 +19,10 @@ func main() {
 		log.Fatalf("Could not connect to the database: %v", err)
 	}
 	defer db.Close()
-	if err := database.ExecuteMigration(db, "000001_create_cardholders_table.up.sql"); err != nil {
-		log.Fatalf("Migration failed: %v", err)
-	}
+	/*
+		if err := database.ExecuteMigration(db, "000001_create_cardholders_table.up.sql"); err != nil {
+			log.Fatalf("Migration failed: %v", err)
+		} */
 	cardholderService := services.NewCardholderService(db)
 	userService := services.NewUserService(db)
 
@@ -33,9 +34,17 @@ func main() {
 	r.HandleFunc("/users", handlers.RegisterHandler(userService)).Methods("POST")
 	r.HandleFunc("/users/login", handlers.LoginHandler(userService)).Methods("POST")
 
-	port := "8080"
-	log.Printf("Server is running on port %s", port)
-	if err := http.ListenAndServe(":"+port, r); err != nil {
+	certFile := "server.crt"
+	keyFile := "server.key"
+	port := "8443" // Preporučuje se koristiti port 443 za HTTPS, ali ovde je 8443 za razvojne potrebe
+	log.Printf("Server is running on https://localhost:%s", port)
+	if err := http.ListenAndServeTLS(":"+port, certFile, keyFile, r); err != nil {
 		log.Fatalf("Could not start server: %v", err)
 	}
+	/*
+		port := "8080"
+		log.Printf("Server is running on port %s", port)
+		if err := http.ListenAndServe(":"+port, r); err != nil {
+			log.Fatalf("Could not start server: %v", err)
+		} */
 }
